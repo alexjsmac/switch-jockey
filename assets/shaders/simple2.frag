bb334e96a467880cfc25f62fe3976fe3f489f22c
Alex MacLean <maclean199@gmail.com>
2020-05-26 18:07:12 -0400

    Shader demo listening to mic input but not updating shader

---
+++ assets/shaders/simple2.frag
@@ -0,0 +1,33 @@
+#include <common>
+
+#if __VERSION__ < 130
+#define TEXTURE2D texture2D
+#else
+#define TEXTURE2D texture
+#endif
+
+#define P 3.14159
+#define E .001
+
+#define T .03 // Thickness
+#define W 2.  // Width
+#define A .09 // Amplitude
+#define V 1.  // Velocity
+
+uniform vec3      iResolution;
+uniform float     iTime;
+uniform sampler2D iChannel0;
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+	vec2 c = fragCoord.xy / iResolution.xy;
+	float s = TEXTURE2D(iChannel0, c * .5).r;
+	c = vec2(0, A*s*sin((c.x*W+iTime*V)* 2.5)) + (c*2.-1.);
+	float g = max(abs(s/(pow(c.y, 2.1*sin(s*P))))*T,
+				  abs(.1/(c.y+E)));
+	fragColor = vec4(g*g*s*.6, g*s*.44, g*g*.7, 1);
+}
+
+void main() {
+  mainImage(gl_FragColor, gl_FragCoord.xy);
+}
--- scripts/shader.js
+++ scripts/shader.js
@@ -1,5 +1,14 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js';
 
+const audioContext = new AudioContext();
+
+// One-liner to resume playback when user interacted with the page.
+document.querySelector('button').addEventListener('click', function() {
+  audioContext.resume().then(() => {
+    console.log('Playback resumed successfully');
+  });
+});
+
 async function loadShader(url) {
   try {
     const response = await fetch(url);
@@ -11,6 +20,13 @@ async function loadShader(url) {
 }
 
 async function main() {
+
+  var microphone_stream = null,
+      gain_node = null,
+      script_processor_fft_node = null,
+      analyser = null,
+      audioData = null;
+
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
   renderer.autoClearColor = false;
@@ -24,17 +40,46 @@ async function main() {
     -1, // near,
      1, // far
   );
+
+  const handleSuccess = function(stream) {
+
+    gain_node = audioContext.createGain();
+    gain_node.connect(audioContext.destination);
+
+    microphone_stream = audioContext.createMediaStreamSource(stream);
+
+    script_processor_fft_node = audioContext.createScriptProcessor(2048, 1, 1);
+    script_processor_fft_node.connect(gain_node);
+
+    analyser = audioContext.createAnalyser();
+    analyser.smoothingTimeConstant = 0;
+    analyser.fftSize = 2048;
+
+    microphone_stream.connect(analyser);
+
+    analyser.connect(script_processor_fft_node);
+
+    script_processor_fft_node.onaudioprocess = function() {
+      audioData = new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(audioData);
+    };
+  };
+
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then(handleSuccess);
+
   let scene = new THREE.Scene();
   const scene1 = new THREE.Scene();
   const scene2 = new THREE.Scene();
   const plane = new THREE.PlaneBufferGeometry(2, 2);
 
-  const fragmentShader1 = await loadShader('assets/shaders/shader1.txt');
-  const fragmentShader2 = await loadShader('assets/shaders/shader2.txt');
+  const fragmentShader1 = await loadShader('assets/shaders/simple2.frag');
+  const fragmentShader2 = await loadShader('assets/shaders/shader2.frag');
 
   const uniforms = {
     iTime: { value: 0 },
     iResolution:  { value: new THREE.Vector3() },
+    iChannel0: { value: audioData }
   };
 
   const material1 = new THREE.ShaderMaterial({
@@ -75,9 +120,10 @@ async function main() {
     const canvas = renderer.domElement;
     uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
     uniforms.iTime.value = time;
+    uniforms.iChannel0.value = audioData;
 
     const slider = document.getElementById('movementRange');
-    if (slider.value > 0.5) {
+    if (slider.value <= 1) {
       if (scene != scene1) {
         scene = scene1;
         changeScene();
