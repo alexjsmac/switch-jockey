76399559acec23c8e8cb50dd8cbabfc4a0513c4f
Alex MacLean <maclean199@gmail.com>
2020-05-29 17:13:59 -0400

    Audio-reactive shader is working!!

--- scripts/simple.js
+++ scripts/simple.js
@@ -9,33 +9,25 @@ let analyser,
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 const fragmentShader = `
-#include <common>
-
-uniform vec3      iResolution;
-uniform float     iTime;
+uniform vec3 iResolution;
+uniform float iTime;
 uniform sampler2D iChannel0;
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-  // Normalized pixel coordinates (from 0 to 1)
-	vec2 uv = fragCoord / iResolution.xy;
-
-	// first texture row is frequency data
-	float fft  = texture2D( iChannel0, vec2(uv.x,0.25) ).x;
-
-    // second texture row is the sound wave
-	float wave = texture2D( iChannel0, vec2(uv.x,0.75) ).x;
-
-	// convert frequency to colors
-	vec3 col = vec3(1.0)*fft;
-
-    // add wave form on top
-	col += 1.0 -  smoothstep( 0.0, 0.01, abs(wave - uv.y) );
-
-    col = pow( col, vec3(1.0,0.5,2.0) );
-
-	// output final color
-	fragColor = vec4(col,1.0);
+  vec3 c;
+  float z = 0.1 * iTime;
+  vec2 uv = fragCoord / iResolution.xy;
+  vec2 p = uv - 0.5;
+  p.x *= iResolution.x / iResolution.y;
+  float l = 0.2 * length(p);
+  for (int i = 0; i < 3; i++) {
+    z += 0.07;
+    uv += p / l * (sin(z) + 1.0) * abs(sin(l * 9.0 - z * 2.0));
+    c[i] = 0.01 / length(abs(mod(uv, 1.0) - 0.5));
+  }
+  float intensity = texture2D(iChannel0, vec2(l, 0.5)).x;
+  fragColor = vec4(c / l * intensity, iTime);
 }
 
 void main() {
@@ -63,27 +55,14 @@ const camera = new THREE.OrthographicCamera(
    1, // far
 );
 
-function copyAudioDataToTexture(audioData) {
-  let textureArray = new Uint8Array(4 * audioData.length)
-  for (let i = 0; i < audioData.length; i++) {
-    textureArray[4 * i + 0] = audioData[i] // R
-    textureArray[4 * i + 1] = audioData[i] // G
-    textureArray[4 * i + 2] = audioData[i] // B
-    textureArray[4 * i + 3] = 255          // A
-  }
-  return textureArray;
-}
-
 function createScene() {
   scene = new THREE.Scene();
-  let texArray = copyAudioDataToTexture(audioData);
-  const texture = new THREE.DataTexture(texArray, audioData.length, 1, THREE.UnsignedByteType);
   const plane = new THREE.PlaneBufferGeometry(2, 2);
 
   uniforms = {
     iTime: { value: 0 },
     iResolution:  { value: new THREE.Vector3() },
-    iChannel0: texture
+    iChannel0: { value: new THREE.DataTexture(audioData, analyser.fftSize/2, 1, THREE.LuminanceFormat) }
   };
 
   const material = new THREE.ShaderMaterial({
@@ -114,12 +93,12 @@ function animate(time){
   resizeRendererToDisplaySize(renderer);
 
   analyser.getByteFrequencyData(audioData);
-  uniforms.iChannel0.image.data = copyAudioDataToTexture(audioData);
 
   time *= 0.001;
-  uniforms.iTime.value = time;
 
+  uniforms.iTime.value = time;
   uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+  uniforms.iChannel0.value.needsUpdate = true;
 
   renderer.render(scene, camera);
 }
@@ -129,11 +108,11 @@ function startMic(){
     navigator.getUserMedia({ audio: true, video: false }, function( stream ) {
 
       analyser = audioCtx.createAnalyser();
-      source = audioCtx.createMediaStreamSource( stream );
+      source = audioCtx.createMediaStreamSource(stream);
 
       source.connect(analyser);
 
-      analyser.fftSize = 512;
+      analyser.fftSize = 1024;
       audioData = new Uint8Array(analyser.frequencyBinCount);
 
       createScene();
